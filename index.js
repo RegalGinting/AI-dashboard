94296df2e8684a3374b1ef178da96519f0b3e0eb
RegalGinting <cerdasginting04@gmail.com>
2025-12-01 15:21:21 +0700

    Index

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+    alert("Login button clicked!");
+});
+
+document.getElementById("signupBtn").addEventListener("click", () => {
+    alert("Signup button clicked!");
+});
